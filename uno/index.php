1c02275b0466358070d631c089530bd2f3489355
Andretreccia <andreabtreccia@gmail.com>
2021-12-02 12:16:15 +0100

    prima bozza delle condizioni per lo svolgimento dello snack 2

---
+++ uno/index.php
@@ -0,0 +1,52 @@
+<?php
+
+$giornata_x = [
+            [
+                $casa = [
+                'nome' => 'squadra1',
+                'punti'=> 22 ,
+            ], 
+
+                $ospite =[
+                  'nome' => 'squadra2',
+                'punti' => 44,
+              ]
+                ],
+                [
+                $casa = [
+                'nome' => 'squadra3',
+                'punti'=> 22 ,
+            ], 
+
+                $ospite =[
+                  'nome' => 'squadra5',
+                'punti' => 15,
+              ]
+            ] ,[
+                $casa = [
+                'nome' => 'squadra8',
+                'punti'=> 55 ,
+            ], 
+
+                $ospite =[
+                  'nome' => 'squadra1',
+                'punti' => 60,
+              ]
+            ]      
+                ];
+ var_dump($giornata_x);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php for ($i=0; $i < 3; $i++) { ?> 
+        
+    <?php } ?>
+</body>
+</html>
